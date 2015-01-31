fcd91073cc5d21a3342f9f36a9e66959ffd8f4ba
sanjay-ak-us <sanjay.ak.us@gmail.com>
2015-01-31 14:52:01 +0100

    unit 5 lesson 1

--- Unit5/ViewController.swift
+++ Unit5/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!;
+    let context = CIContext(options:nil);
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func chooseExistingPhoto(){
+        let picker = UIImagePickerController();
+        
+        picker.delegate = self;
+        picker.sourceType = .PhotoLibrary;
+        self.presentViewController(picker, animated: true, completion: nil);
     }
-
-
+    
+    @IBAction func takeNewPicture(sender: UIButton) {
+        let picker = UIImagePickerController();
+        picker.delegate = self;
+        picker.sourceType = .Camera;
+        self.presentViewController(picker, animated: true, completion: nil);
+    }
+    
+    @IBAction func applyFiler(sender: AnyObject) {
+        // Create an image to filter
+        let inputImage = CIImage(image: imageView.image)
+        
+        // Create a random color to pass to a filter
+        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
+        
+        // Apply a filter to the image
+        let filteredImage = inputImage.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+        
+        // Render the filtered image
+        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent());
+        
+        // Reflect the change back in the interface
+        imageView.image = UIImage(CGImage: renderedImage)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        imageView.image = image;
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
-
